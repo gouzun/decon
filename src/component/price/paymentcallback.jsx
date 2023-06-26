86f5da3ebcb70f5872e10af33378b241519ebbb3
gouzun <gouzun1@gmail.com>
2023-06-26 17:32:47 +0800

    26-6-23

--- src/component/price/paymentcallback.jsx
+++ src/component/price/paymentcallback.jsx
@@ -2,24 +2,29 @@ import React from 'react'
 import Header from "../header/header.component";
 import Footer from "../footer/footer.component";
 import { useEffect } from 'react';
-
+git ini
 import { BUTTONCOLOR, LABELCOLOR, LABELHOVERCOLOR, BGCOLOR } from '../../utils/theme';
 
 const PaymentCallback = () => {
     useEffect(() => {
         const handleCallback = async () => {
             try {
-                console.log('payment-callback');
-                // Get the payment details from the callback request
-                // const response = await fetch('/api/payment-callback'); // Replace with the actual server-side endpoint URL
+                console.log('payment-redirect');
+                const urlParams = new URLSearchParams(window.location.search);
+                const billplzId = urlParams.get('billplz[id]');
+                const isPaid = urlParams.get('billplz[paid]');
+                const paidAt = urlParams.get('billplz[paid_at]');
+                const transactionId = urlParams.get('billplz[transaction_id]');
+                const transactionStatus = urlParams.get('billplz[transaction_status]');
+                const xSignature = urlParams.get('billplz[x_signature]');
 
-                // if (!response.ok) {
-                //     throw new Error('Failed to fetch payment details');
-                // }
-
-                // // Parse the response data as JSON
-                // const paymentData = await response.json();
-                // console.log('paymentData:',paymentData);
+                // You can now use the retrieved data as needed
+                console.log(billplzId);
+                console.log(isPaid);
+                console.log(paidAt);
+                console.log(transactionId);
+                console.log(transactionStatus);
+                console.log(xSignature);
             } catch (error) {
                 console.log(error)
             }
