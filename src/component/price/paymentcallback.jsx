08013029ad36a1ecfd956ca617a57172be8cfd62
gouzun <gouzun1@gmail.com>
2023-06-27 16:53:25 +0800

    27-6-23

--- src/component/price/paymentcallback.jsx
+++ src/component/price/paymentcallback.jsx
@@ -2,10 +2,45 @@ import React from 'react'
 import Header from "../header/header.component";
 import Footer from "../footer/footer.component";
 import { useEffect } from 'react';
-git ini
+import crypto from 'crypto';
+
+
 import { BUTTONCOLOR, LABELCOLOR, LABELHOVERCOLOR, BGCOLOR } from '../../utils/theme';
 
 const PaymentCallback = () => {
+    const calculateXSignature = async (params, apiKey) => {
+        // Sort the parameters alphabetically by key
+        const sortedParams = Object.keys(params).sort().reduce((acc, key) => {
+            acc[key] = params[key];
+            return acc;
+        }, {});
+
+        // Create a new TextEncoder instance
+        const encoder = new TextEncoder();
+
+        // Convert the sortedParams to a string
+        const paramString = JSON.stringify(sortedParams);
+
+        // Encode the string into a Uint8Array
+        const encodedData = encoder.encode(paramString);
+
+        // Import the API key as a CryptoKey
+        const keyData = encoder.encode(apiKey);
+        const importedKey = await crypto.subtle.importKey('raw', keyData, { name: 'HMAC', hash: 'SHA-256' }, false, ['sign']);
+
+        // Calculate the HMAC-SHA256 hash using the imported key
+        const signature = await crypto.subtle.sign('HMAC', importedKey, encodedData);
+
+        // Convert the signature to a hexadecimal string
+        const hexSignature = Array.from(new Uint8Array(signature))
+            .map((byte) => byte.toString(16).padStart(2, '0'))
+            .join('');
+
+        return hexSignature;
+    };
+
+
+
     useEffect(() => {
         const handleCallback = async () => {
             try {
@@ -19,12 +54,26 @@ const PaymentCallback = () => {
                 const xSignature = urlParams.get('billplz[x_signature]');
 
                 // You can now use the retrieved data as needed
-                console.log(billplzId);
-                console.log(isPaid);
-                console.log(paidAt);
-                console.log(transactionId);
-                console.log(transactionStatus);
-                console.log(xSignature);
+                console.log('billplzId:', billplzId);
+                console.log('isPaid:', isPaid);
+                console.log('paidAt:', paidAt);
+                console.log('transactionId:', transactionId);
+                console.log('transactionStatus:', transactionStatus);
+                console.log('xSignature:', xSignature);
+
+                const hexSignature = calculateXSignature(
+                    {
+                        id: billplzId,
+                        paid: isPaid,
+                        paid_at: paidAt,
+                        transaction_id: transactionId,
+                        transaction_status: transactionStatus,
+                    },
+                    process.env.API_KEY
+                );
+                console.log('hexSignature:', hexSignature);
+                console.log('xSignature:', xSignature);
+
             } catch (error) {
                 console.log(error)
             }
